9b53090f7f6268eb6d6c95030c066b6e75358dc0
matthias <79849301+Tolviere@users.noreply.github.com>
2025-02-22 12:05:21 -0500

    Add basic css for calc

---
+++ script.js
@@ -0,0 +1,32 @@
+let num1;
+let operator;
+let num2;
+
+function operate(a, b, op) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
