ccff3cbef124c400e5610070a871d1905c1f3f07
matthias <79849301+Tolviere@users.noreply.github.com>
2025-02-22 12:58:10 -0500

    Add basic calc functionality

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
-let num1;
-let operator;
-let num2;
+let num1 = '';
+let operator = '';
+let num2 = '';
+let ans = null;
+
+const display = document.querySelector('.display');
+const buttonContainer = document.querySelector('.button-container');
+buttonContainer.addEventListener('click', e => {
+    if (e.target.parentNode.className === 'button-row') {
+        let btn = e.target;
+
+        switch(btn.className) {
+            case 'num':
+                if (operator === '') {
+                    ans = null;
+                    num1 += btn.textContent;
+                    display.textContent = num1;
+                } else {
+                    num2 += btn.textContent;
+                    display.textContent = num2;
+                }
+                break;
+
+            case 'op':
+                if (num1 !== '' && num2 !== '') {
+                    ans = operate(num1, num2, operator);
+                    display.textContent = ans;
+                    resetInput();
+                } 
+                if (ans && num1 === '') {
+                    num1 = ans;
+                }
+                operator = btn.textContent;
+                break;
+
+            case 'equals':
+                ans = operate(num1, num2, operator);
+                display.textContent = ans;
+                resetInput();
+                break;
+            
+            case 'clear':
+                resetInput();
+                display.textContent = '';
+        }
+    }
+});
+
+function resetInput() {
+    operator = '';
+    num1 = '';
+    num2 = '';
+}
 
 function operate(a, b, op) {
+    a = +a;
+    b = +b;
+
     switch(op) {
         case '+':
             return add(a, b);
